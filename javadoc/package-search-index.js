7cf2b1e540edf733174d48ef052a614f31ca3b26
HAKKI <HAKKI@HAKKI-PC.home>
2020-11-20 13:55:52 +0100

    Added Javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"nl.svb.leaseacarapi.leasecalculationservice.bean"},{"l":"nl.svb.leaseacarapi.leasecalculationservice.entity"},{"l":"nl.svb.leaseacarapi.leasecalculationservice.proxy"},{"l":"nl.svb.leaseacarapi.leasecalculationservice.repository"}]
