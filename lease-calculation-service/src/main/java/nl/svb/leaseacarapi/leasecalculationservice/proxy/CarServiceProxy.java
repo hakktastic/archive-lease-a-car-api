2bc52a02bc6fee172cbaf263d20b57a84cc68b70
hakktastic <hakktastic@gmail.com>
2020-11-27 15:20:18 +0100

    Zuul Api Gateway implementation

--- lease-calculation-service/src/main/java/nl/svb/leaseacarapi/leasecalculationservice/proxy/CarServiceProxy.java
+++ lease-calculation-service/src/main/java/nl/svb/leaseacarapi/leasecalculationservice/proxy/CarServiceProxy.java
@@ -11,10 +11,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  * Proxy for retrieving Car Entities from Car-Service through Feign.
  *
+ * <p>
+ * The request is executed through the Zuul API Gateway by pointing the Feign client to the gateway
+ * <b>{@code netflix-zuul-api-gateway-server}</b> instead of the service itself.
+ * </p>
+ *
  * @author HAKKI CABUK
  *
  */
-@FeignClient(name = "car-service")
+
+// Multiple Feign clients require a {@code contextId} annotation attribute
+@FeignClient(contextId = "car-service", name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "car-service")
 public interface CarServiceProxy {
 
@@ -24,6 +31,6 @@ public interface CarServiceProxy {
    * @param id Id of the Car object
    * @return Returns a {@link CarBean} object containing car data
    */
-  @GetMapping("/cars/{id}")
+  @GetMapping("/car-service/cars/{id}")
   Optional<CarBean> getCarById(@PathVariable int id);
 }
