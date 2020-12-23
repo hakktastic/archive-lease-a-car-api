da6e1c9b58c8e38669ffe568973a96c199458c0c
hakktastic <49901606+hakktastic@users.noreply.github.com>
2020-12-23 16:25:32 +0100

    -

---
+++ interest-rate-calculation-service/src/test/java/nl/svb/leaseacarapi/leasecalculationservice/InterestRateCalculationServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package nl.svb.leaseacarapi.leasecalculationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterestRateCalculationServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
