0a5b828b71ec219c867419ddcdaea95f4aab49d6
hakktastic <49901606+hakktastic@users.noreply.github.com>
2020-12-23 16:25:48 +0100

    -

--- singlemodule-lease-a-car-api/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"nl.svb.leaseacarapi.leasecalculationservice.bean"},{"l":"nl.svb.leaseacarapi.leasecalculationservice.entity"},{"l":"nl.svb.leaseacarapi.leasecalculationservice.proxy"},{"l":"nl.svb.leaseacarapi.leasecalculationservice.repository"}]
--- singlemodule-lease-a-car-api/launch-zipkin-server.cmd
+++
@@ -1,2 +0,0 @@
-SET RABBIT_URI=amqp://localhost 
-java -jar C:\apps\zipkin-server\zipkin-server-2.23.0-exec.jar
